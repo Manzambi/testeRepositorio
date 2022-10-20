a936f6fa8e8e2479effc6281b07cc91b440a6b12
Mac Pro <barby@Nicks-bigzzag-gomez.local>
2022-10-20 09:50:49 +0200

    las etiquetas html css y ts

---
+++ app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  title = 'etsto '
+  Website = 'http://wwww.faztudo.com'
+  estudiantes =['Andre', 'Antonio', 'Mario']
+  provar = true
+  mostrar = 'ocultar'
+  Nombre = 'Name'
+
+constructor(){
+  
+}
+
+salvarDatos(valor:string){
+  console.log(valor)
+  this.estudiantes.push(valor)
+}
+
+MostarOcultar(){
+  this.provar = !this.provar
+  // if(this.provar== false)
+    //   this.mostrar = 'mostar'
+      // else this.mostrar='ocultar'
+
+}
+
+
+}
